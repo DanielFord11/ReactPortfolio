f1e17ba7e09bd1cacf41774bcbe058121438ca6f
Danny Ford <dannyford11@me.com>
2023-10-08 10:45:49 -0600

    setting up initial react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* Add other components and content here */}
+    </div>
+  );
+}
+
+export default App;

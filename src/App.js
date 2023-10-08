d26ad2eff6b72886a3072b1682958ccdc0d91f57
Danny Ford <dannyford11@me.com>
2023-10-08 11:03:12 -0600

    adding profile component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Navbar from './Navbar';
+import './App.css'; // You can import your app-wide styles here
+import Navbar from './Navbar'; // Import the Navbar component
+import Profile from './components/Profile'; // Import the Profile component
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar /> {/* Render the Navbar component */}
+      <Profile /> {/* Render the Profile component */}
       {/* Add other components and content here */}
     </div>
   );

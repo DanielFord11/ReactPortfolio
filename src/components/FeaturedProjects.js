e8f75b883c8e885fa273855e2e7265421cada94a
Danny Ford <dannyford11@me.com>
2023-10-14 16:03:08 -0600

    added project images

--- src/components/FeaturedProjects.js
+++ src/components/FeaturedProjects.js
@@ -1,43 +1,71 @@
 import React from 'react';
-import './FeaturedProjects.css'; // You can create a CSS file for styling
+import './FeaturedProjects.css';
+import'../Assets/Health.png'
+import healthImage from '../Assets/Health.png';
+import ufoImage from '../Assets/UFOs.png';
+import refugeeImage from '../Assets/Refugee.png';
+import collegeImage from '../Assets/college.png';
+import ghostWriterImage from '../Assets/ghostWriter.png';
 
 function FeaturedProjects() {
   return (
     <section className="Featured_Projects_Section">
       <div className="Featured_Projects">
         <h1 id="Project_Landing">03. Some Things I've Built</h1>
-
         <div className="Project_1">
-          <h1>UFO Semantic Similarity</h1>
-          <a href="https://danielford11.github.io/Project-2/">
-            <img src="Assets/UFO_ML.png" alt="UFO ML Project IMG" />
-          </a>
-          <span>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
-              cumque sed magni id harum adipisci, earum blanditiis culpa accusamus distinctio! Debitis facere laborum
-              tenetur alias?
-            </p>
-          </span>
-        </div>
-
-        <div className="Project_2">
-          <h1>Weather Analysis</h1>
-          <a href="https://danielford11.github.io/HTML_CSS_Dashboard/wind_speed.html">
-            <img src="Assets/Weather_Analysis.png" alt="Plotly Graphs Juxtaposing Weather Variables" />
-          </a>
-          <span>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
-              cumque sed magni id harum adipisci, earum blanditiis culpa accusamus distinctio! Debitis facere laborum
-              tenetur alias?
-            </p>
-          </span>
-        </div>
-
-        <div className="Project_3">
+           <h1>AI Generated News Site</h1>
+           <a href="https://danielford11.github.io/Project-2/"></a>
+           <img id="NewSite_Photo" src={ghostWriterImage} alt="New Site Photo" height="620rem" />
+           <span>
+             <p>
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
+               cumque sed magni id harum adipisci, earum blanditiis culpa accusamus distinctio! Debitis facere laborum
+               tenetur alias?
+             </p>
+           </span>
+         </div>
+         <div className="Project_2">
+           <h1>College Calculator</h1>
+           <a href="https://danielford11.github.io/Project-2/"></a>
+           <img id="College_Photo" src={collegeImage} alt="College Data Photo" height="620rem" />
+           <span>
+             <p>
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
+               cumque sed magni id harum adipisci, earum blanditiis culpa accusamus distinctio! Debitis facere laborum
+               tenetur alias?
+             </p>
+           </span>
+         </div>
+         <div className="Project_3">
+           <h1>UFO Sighitings NLP Semantic Similarity</h1>
+           <a href="https://danielford11.github.io/Project-2/"></a>
+           <img id="UFO_Photo" src={ufoImage} alt="UFO Data Photo" height="320rem" />
+           <span>
+             <p>
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
+               cumque sed magni id harum adipisci, earum blanditiis culpa accusamus distinctio! Debitis facere laborum
+               tenetur alias?
+             </p>
+           </span>
+         </div>
+        <div className="Project_4">
           <h1>Refugee Analysis</h1>
-          <img src="#" alt="UFO NLP Machine Learning" />
+          <a href="https://danielford11.github.io/Project-2/">
+          <img id="UFO_Photo" src={refugeeImage} alt="Refugee Data Photo" height="320rem" />
+          </a>
+          <span>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
+              cumque sed magni id harum adipisci, earum blanditiis culpa accusamus distinctio! Debitis facere laborum
+              tenetur alias?
+            </p>
+          </span>
+        </div>
+        <div className="Project_5">
+          <h1>d3 JS Health Visualizations</h1>
+          <a href="https://danielford11.github.io/Project-2/">
+          <img id="Health_Photo" src={healthImage} alt="Health Data Photo" height="320rem" />
+          </a>
           <span>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam esse, hic quaerat consequuntur beatae
